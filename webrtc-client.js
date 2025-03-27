4e71038e8fbbf17d4f2e39b914191a523c6ca6f5
edwinchu0711 <162130385+edwinchu0711@users.noreply.github.com>
2025-03-27 16:07:05 +0800

    Update webrtc-client.js

--- webrtc-client.js
+++ webrtc-client.js
@@ -18,6 +18,7 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
     localStream = stream;
     localVideo.srcObject = stream;
     stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+    console.log('Local stream added');
   })
   .catch(error => {
     console.error('Error accessing media devices:', error);
@@ -28,35 +29,46 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
 peerConnection.ontrack = event => {
   if (!remoteVideo.srcObject) {
     remoteVideo.srcObject = event.streams[0];
+    console.log('Remote stream added');
   }
 };
 
 // Signaling logic (using WebSocket)
-const signalingSocket = new WebSocket('wss://superficial-obsidian-harp.glitch.me');
+const signalingSocket = new WebSocket('ws://aluminum-tremendous-archaeology.glitch.me');
+
+signalingSocket.onopen = () => {
+  console.log('WebSocket connection established');
+};
 
 signalingSocket.onmessage = async message => {
+  console.log('Received message:', message.data);
   const data = JSON.parse(message.data);
 
   if (data.type === 'offer') {
+    console.log('Received offer');
     await peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer));
     const answer = await peerConnection.createAnswer();
     await peerConnection.setLocalDescription(answer);
     signalingSocket.send(JSON.stringify({ type: 'answer', answer, room: roomName }));
   } else if (data.type === 'answer') {
+    console.log('Received answer');
     await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer));
   } else if (data.type === 'candidate') {
+    console.log('Received ICE candidate');
     await peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
   }
 };
 
 peerConnection.onicecandidate = event => {
   if (event.candidate) {
+    console.log('Sending ICE candidate');
     signalingSocket.send(JSON.stringify({ type: 'candidate', candidate: event.candidate, room: roomName }));
   }
 };
 
 // Join room
 joinRoomButton.onclick = () => {
+  console.log('Join Room button clicked');
   roomName = roomInput.value.trim();
   if (!roomName) {
     alert('Please enter a room name.');
@@ -71,6 +83,7 @@ joinRoomButton.onclick = () => {
 
 // Start call
 startCallButton.onclick = async () => {
+  console.log('Start Call button clicked');
   const offer = await peerConnection.createOffer();
   await peerConnection.setLocalDescription(offer);
   signalingSocket.send(JSON.stringify({ type: 'offer', offer, room: roomName }));
@@ -80,6 +93,7 @@ startCallButton.onclick = async () => {
 
 // End call
 endCallButton.onclick = () => {
+  console.log('End Call button clicked');
   peerConnection.close();
   signalingSocket.close();
   startCallButton.disabled = false;
